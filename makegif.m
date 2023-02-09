71548181b580aa24e5123d4bc3265527fcfbd5be
bednarcyk <79326281+bednarcyk@users.noreply.github.com>
2023-02-09 11:29:08 -0500

    Update makegif.m

--- makegif.m
+++ makegif.m
@@ -1,23 +1,27 @@
 % -- Written by Mathew Schey, U Mass Lowell student, Dec. 2021
-h = figure;
+sz = get(0, 'screensize');
+h = figure("position",[sz(1)+100, sz(2)+200, 0.9*sz(3)+100, sz(4)/2+200]);
 axis tight manual % this ensures that getframe() returns a consistent size
 filename = 'failure.gif';
-a = dir('Damage Snapshot*.bmp');
-a = struct2table(a);
-a = sortrows(a,'date');
-a = table2struct(a);
+a1 = dir('Damage Snapshot*.bmp');
+[xx, order] = sort([a1(:).datenum],'ascend');
+a = a1(order);
 for n = 1:size(a,1)
+    
     I = imread(a(n).name);
-    imshow(I) 
-    frame = getframe(h); 
-    im = frame2im(frame); 
-    [imind,cm] = rgb2ind(im,256);
-    % Write to the GIF File 
-    if n == 1 
-        imwrite(imind,cm,filename,'gif', 'Loopcount',inf,'DelayTime',1); 
-    else 
-        imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',1); 
-    end 
+    imshow(I)
+    frame = getframe(h);
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,256); % -- Matlab
+    % [imind,cm] = rgb2ind(im); % -- Octave
+    
+    % Write to the GIF File
+    if n == 1
+        imwrite(imind,cm,filename,'gif', 'Loopcount',inf,'DelayTime',1);
+    else
+        imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',1);
+    end
+    
 end
- 
+close(h);
 fclose('all');
